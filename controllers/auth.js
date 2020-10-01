91d1431c303a743775c30368f1c81a66b78bb388
heth0218 <hethgala@gmail.com>
2020-10-01 15:49:51 +0530

    User details and apssword update routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,6 +69,42 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ succes: true, data: user });
 });
 
+//@Desc     Update user details 
+//@route    PUT /api/v1/auth/updatedetails
+//@access   private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ succes: true, data: user });
+});
+
+//@Desc     Update password 
+//@route    PUT /api/v1/auth/updatepassword
+//@access   private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+
+  const user = await User.findById(req.user.id).select('+password');
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is incorrect', 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 //@Desc     Forgot password
 //@route    POST /api/v1/auth/forgotpassword
 //@access   public
