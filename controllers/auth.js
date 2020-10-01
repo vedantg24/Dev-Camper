5e6c91b64d93c2b1539127d6a727ddc07c61d6d5
Shashank Gandhi <shashank.gandhi@moneytor.in>
2020-10-01 15:46:13 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,198 +1,164 @@
-const crypto = require('crypto');
-const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middleware/async');
-const sendEmail = require('../utils/sendEmail');
-const User = require('../models/User');
+const crypto = require("crypto");
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const sendEmail = require("../utils/sendEmail");
+const User = require("../models/User");
 
-//@Desc     Register user 
+//@Desc     Register user
 //@route    POST /api/v1/auth/register
 //@access   public
 exports.register = asyncHandler(async (req, res, next) => {
-    const{ name, email, password, role } = req.body;
+  const { name, email, password, role } = req.body;
 
-    //Create user
-    const user= await User.create({
-        name,
-        email,
-        password,
-        role
-    });
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
 
-    sendTokenResponse(user, 200, res);
+  sendTokenResponse(user, 200, res);
 });
 
 //@Desc     Login
 //@route    POST /api/v1/auth/login
 //@access   public
 exports.login = asyncHandler(async (req, res, next) => {
-    const{ email, password } = req.body;
+  const { email, password } = req.body;
 
-    //Validate email and password
-    if(!email || !password) {
-        return next(new ErrorResponse('Please add a email and a password', 400));
-    }
+  //Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please add a email and a password", 400));
+  }
 
-    //Check for user
-    const user = await User.findOne({ email }).select('+password');
+  //Check for user
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-    //Check if password matches
-    const isMatch= await user.matchPassword(password);
+  //Check if password matches
+  const isMatch = await user.matchPassword(password);
 
-    if(!isMatch) {
-        return next(new ErrorResponse('Invalid credentials', 401));
-    }
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-    sendTokenResponse(user, 200, res);
-});   
+  sendTokenResponse(user, 200, res);
+});
 
-//@Desc     Log user out/ clear cookie 
+//@Desc     Log user out/ clear cookie
 //@route    GET /api/v1/auth/logout
 //@access   public
 exports.logout = asyncHandler(async (req, res, next) => {
-
-    res.cookie('token', 'none', {
-        expires: new Date(Date.now() + 1 * 1000),
-        httpOnly: true
-    });
-    res.status(200).json({ succes:true, data: {}});
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 1 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ succes: true, data: {} });
 });
 
-//@Desc     Get current logged in user 
+//@Desc     Get current logged in user
 //@route    POST /api/v1/auth/me
 //@access   private
 exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json({ succes:true, data: user});
-});
-
-//@Desc     Update user details 
-//@route    PUT /api/v1/auth/updatedetails
-//@access   private
-exports.updateDetails = asyncHandler(async (req, res, next) => {
-
-    const fieldsToUpdate = {
-        name: req.body.name,
-        email: req.body.email
-    }
-
-    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
-        new: true,
-        runValidators: true
-    });
-
-   res.status(200).json({ succes:true, data: user});
-});
-
-//@Desc     Update password 
-//@route    PUT /api/v1/auth/updatepassword
-//@access   private
-exports.updatePassword = asyncHandler(async (req, res, next) => {
-
-    const user = await User.findById(req.user.id).select('+password');
-
-    //Check current password
-    if(!(await user.matchPassword(req.body.currentPassword))) {
-         return next(new ErrorResponse('Password is incorrect', 401));
-    }
-
-    user.password= req.body.newPassword;
-    await user.save();
-
-    sendTokenResponse(user, 200, res);
+  res.status(200).json({ succes: true, data: user });
 });
 
 //@Desc     Forgot password
 //@route    POST /api/v1/auth/forgotpassword
 //@access   public
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
-    const user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
 
-    if(!user) {
-        return next(new ErrorResponse('There is no user with that email', 404));
-    }
+  if (!user) {
+    return next(new ErrorResponse("There is no user with that email", 404));
+  }
 
-    //Get reset token
-    const resetToken = user.getResetPasswordToken();
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    });
+
+    res.status(200).json({ success: true, data: "Email sent" });
+  } catch (err) {
+    console.log(err);
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
     await user.save({ validateBeforeSave: false });
 
-    // Create reset url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`; 
-
-    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
-
-    try {
-        await sendEmail({
-            email: user.email,
-            subject: 'Password reset token',
-            message
-        });
-
-        res.status(200).json({ success: true, data: 'Email sent'});
-    } catch (err) {
-        console.log(err);
-
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpire = undefined;
-
-        await user.save({ validateBeforeSave: false });
-
-        return next(new ErrorResponse('Email could not be sent', 500));
-    }
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
 });
 
 // @desc      Reset password
 // @route     PUT /api/v1/auth/resetpassword/:resettoken
 // @access    Public
 exports.resetPassword = asyncHandler(async (req, res, next) => {
-    // Get hashed token
-    const resetPasswordToken = crypto
-      .createHash('sha256')
-      .update(req.params.resettoken)
-      .digest('hex');
-    
-    console.log(resetPasswordToken);
-    const user = await User.findOne({
-      resetPasswordToken,
-      resetPasswordExpire: { $gt: Date.now() }
-    });
-    
-    if (!user) {
-      return next(new ErrorResponse('Invalid token', 400));
-    }
-  
-    // Set new password
-    user.password = req.body.password;
-    user.resetPasswordToken = undefined;
-    user.resetPasswordExpire = undefined;
-    await user.save();
-  
-    sendTokenResponse(user, 200, res);
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resettoken)
+    .digest("hex");
+
+  console.log(resetPasswordToken);
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
   });
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid token", 400));
+  }
+
+  // Set new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
 
 // Get token from model,create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
+  const token = user.getSignedJwtToken();
 
-    //Create token
-    const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    //cookie only accessed through client side script
+    httpOnly: true,
+  };
 
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        //cookie only accessed through client side script
-        httpOnly: true  
-    };
+  if (process.env.NODE_ENV === "production") {
+    options = true;
+  }
 
-    if(process.env.NODE_ENV === 'production') {
-        options = true;
-    } 
-
-    res
+  res
     .status(statusCode)
-    .cookie('token', token, options)
-    .json({ success:  true, token});
+    .cookie("token", token, options)
+    .json({ success: true, token });
 };
